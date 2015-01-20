d437abd4be59556446d2e50df24743a77c6e5eaf
RK <brk0018@gmail.com>
2015-01-20 14:57:30 +0530

    Added Jobs CRUD ability

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -22,9 +22,19 @@ class JobsController < ApplicationController
 	end
 
 	def update
+		if @job.update(jobs_params)
+			redirect_to @job
+		else
+			render "Edit"
+		end
 	end
 
 	def edit
+	end
+
+	def destroy
+		@job.destroy
+		redirect_to root_path
 	end
 
 	private
